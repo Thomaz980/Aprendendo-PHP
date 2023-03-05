963b344431ce668c35fa6f77f2e744ff8408f6bc
Thomaz980 <112140581+Thomaz980@users.noreply.github.com>
2023-03-05 14:46:20 -0300

    salvando os arquivos

---
+++ ex_00/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html long="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-acale=1.0">
+	<title>Exemplo PHP</title>
+</head>
+	
+<body>
+	<h1>Exemplo PHP</h1>
+	<?php
+		$name = "Thomaz Santos";
+		echo "<p>O meu nome é $name</p>";
+	?>
+</body>
+</html>
---
+++ ex_06/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aprendendo Variáveis</title>
+</head>
+<body>
+    <?php
+    #comentário!
+    //comentário!
+
+    $nome = "Maria";
+    $sobrenome = "Santos";
+    const PAIS = "Brasil";
+
+    $nome = "João";
+    
+    echo "Muito prazer, $nome $sobrenome! Você mora no " .PAIS ;
+
+    $idade = 43;
+    $peso = 118.5;
+    $casado = false;
+    ?>
+</body>
+</html>
